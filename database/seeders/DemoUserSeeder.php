90e13fa315045f9e9b2d30e6fd4e3022cdd68321
snekzleberg <snekzleberg3@gmail.com>
2026-03-20 02:42:07 +0000

    Add DemoUserSeeder to create default admin and customer users
    - Created a new seeder class `DemoUserSeeder` to populate the database with default users.
    - The seeder includes an admin user with email 'admin@demo.com' and a customer user with email 'user@demo.com'.
    - Both users have a default password set to 'password'.

---
+++ database/seeders/DemoUserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DemoUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin user
+        User::updateOrCreate(
+            ['email' => 'admin@demo.com'],
+            [
+                'name' => 'Admin Demo',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        // Customer user
+        User::updateOrCreate(
+            ['email' => 'user@demo.com'],
+            [
+                'name' => 'User Demo',
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
